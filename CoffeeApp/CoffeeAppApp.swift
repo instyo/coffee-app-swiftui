c4d6a56680612a8098e5d3f661087c02d571b92f
Ikhwan <nsetyo19@gmail.com>
2022-11-19 19:34:51 +0700

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Ikhwan on 19/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
